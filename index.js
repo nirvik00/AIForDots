5b79b7a4f5e114627caef64e77fd51760b68f037
nirvik saha <nirviksaha@nirviks-MacBook-Air.local>
2019-08-12 19:14:23 -0400

    port set for production

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/games/asteroidsAI', (req, res)=>{
     res.render('games/asteroidsAI');
 });
 
-const port=5000;
+const port=process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`server started on port ${port}`);
 });
